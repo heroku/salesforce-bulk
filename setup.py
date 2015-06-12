97ead53c9b4c226a8263572fef6cec153d141edb
Scott Persinger <scottpersinger@gmail.com>
2015-06-12 12:26:24 -0700

    Bump to 1.0.8 with Upsert and specified version support

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('LICENSE') as f:
 
 setup(
     name='salesforce-bulk',
-    version='1.0.7',
+    version='1.0.8',
     description='Python interface to the Salesforce.com Bulk API.',
     long_description=readme,
     author='Scott Persinger',
