c7b2e55988b8bcf5f46951f474ae0bdf326c5551
Chris Lambacher <clambacher@heroku.com>
2017-08-31 15:36:46 -0400

    Prep for 2.0.0 release

--- salesforce_bulk/__init__.py
+++ salesforce_bulk/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
 from .salesforce_bulk import SalesforceBulk, BulkApiError, UploadResult
 from .csv_adapter import CsvDictsAdapter
-
-__version__ = '2.0.0dev6'
+from .__version__ import __version__
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 from __future__ import absolute_import
+
+import io
 import os
 import sys
+import errno
+from shutil import rmtree
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, Command
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -25,16 +26,57 @@ requires = [
 
 ]
 
-with open('README.md') as f:
-    readme = f.read()
-with open('LICENSE') as f:
-    license = f.read()
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, 'salesforce_bulk', '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds...')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise
+
+        self.status('Building Source and Wheel (universal) distribution...')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine...')
+        os.system('twine upload dist/*')
+
+        sys.exit()
 
 setup(
     name='salesforce-bulk',
-    version='2.0.0dev6',
+    version=about['__version__'],
     description='Python interface to the Salesforce.com Bulk API.',
-    long_description=readme,
+    long_description=long_description,
     author='Scott Persinger',
     author_email='scottp@heroku.com',
     url='https://github.com/heroku/salesforce-bulk',
@@ -42,7 +84,7 @@ setup(
     package_data={'': ['LICENSE']},
     include_package_data=True,
     install_requires=requires,
-    license=license,
+    license='MIT',
     zip_safe=False,
     classifiers=(
         'Development Status :: 5 - Production/Stable',
@@ -50,7 +92,6 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
